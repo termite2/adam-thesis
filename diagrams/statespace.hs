e605377e4e3f2d1468223ef2e246472e826db220
Adam Walker <adamwalker10@gmail.com>
2014-12-01 20:37:58 +1100

    statespace diagram work

--- diagrams/statespace.hs
+++ diagrams/statespace.hs
@@ -10,10 +10,14 @@ concrete =  hrule 80 # moveTo (p2 (20, 0)) # dashingN [0.03, 0.03] 0
          <> hrule 80 # moveTo (p2 (20, -40)) # dashingN [0.03, 0.03] 0
          <> vrule 80 # moveTo (p2 (0, -20))  # dashingN [0.03, 0.03] 0
          <> vrule 80 # moveTo (p2 (40, -20))  # dashingN [0.03, 0.03] 0
-labels   =  arrowBetween' (with & arrowTail .~ spike') (p2 (-20, 30)) (p2 (60, 30))
-         <> arrowBetween' (with & arrowTail .~ spike') (p2 (70, 20)) (p2 (70, -60))
-varNames =  text "X" # fontSize (Local 10) # moveTo (p2 (20, 40))
-         <> text "Y" # fontSize (Local 10) # moveTo (p2 (80, -20))
+labels   =  arrowBetween' (with & arrowTail .~ spike') (p2 (-20, 40)) (p2 (60, 40))
+         <> arrowBetween' (with & arrowTail .~ spike') (p2 (80, 20)) (p2 (80, -60))
+         <> arrowBetween' (with & arrowTail .~ spike') (p2 (20, 25)) (p2 (60, 25))
+         <> arrowBetween' (with & arrowTail .~ spike') (p2 (65, 20)) (p2 (65, -20))
+varNames =  text "X" # fontSize (Local 10) # moveTo (p2 (20, 50))
+         <> text "Y" # fontSize (Local 10) # moveTo (p2 (90, -20))
+         <> text "U" # fontSize (Local 6) # moveTo (p2 (40, 30))
+         <> text "V" # fontSize (Local 6) # moveTo (p2 (70, 0))
 concState = square 20 # fc gray # moveTo (p2 (50, 10)) # lw none
 diag     = concState <> concrete <> abstract <> labels <> varNames
 
