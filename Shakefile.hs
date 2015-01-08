80eb41a216c1fa1ad54634dd89b10a96b0e3fb54
Adam Walker <adamwalker10@gmail.com>
2015-01-08 18:37:12 +1300

    bibtex

--- Shakefile.hs
+++ Shakefile.hs
@@ -10,6 +10,9 @@ main = shakeArgs shakeOptions{shakeFiles=".shake/"} $ do
     "thesis.pdf" %> \out -> do
         files <- getDirectoryFiles "diagrams" ["//*.hs"]
         need $ ["thesis.tex", "background.tex", "game.tex", "intro.tex", "solving.tex", "syntcomp.tex", "userguided.tex"] ++ map (\x -> "diagrams" </> x -<.> "pdf") files
+        () <- cmd "pdflatex" ["thesis.tex"]
+        () <- cmd "bibtex"   ["thesis.aux"]
+        () <- cmd "pdflatex" ["thesis.tex"]
         cmd "pdflatex" ["thesis.tex"]
 
     "diagrams/*.pdf" %> \out -> do
