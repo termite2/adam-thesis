8975e3f6a2ac82ecfa57f2a760e773e3a0916d47
Adam Walker <adamwalker10@gmail.com>
2015-01-08 17:35:48 +1300

    build diagrams

--- Shakefile.hs
+++ Shakefile.hs
@@ -8,6 +8,15 @@ main = shakeArgs shakeOptions{shakeFiles=".shake/"} $ do
     want ["thesis.pdf"]
 
     "thesis.pdf" %> \out -> do
-        need ["thesis.tex"]
+        files <- getDirectoryFiles "diagrams" ["//*.hs"]
+        need $ "thesis.tex" : map (\x -> "diagrams" </> x -<.> "pdf") files
         cmd "pdflatex" ["thesis.tex"]
 
+    "diagrams/*.pdf" %> \out -> do
+        let nd = out -<.> "hs"
+        need $ [nd]
+        cmd "runhaskell" [nd, "-o", out, "-w 200"]
+
+    phony "clean" $ do
+        putNormal "Cleaning files in .shake"
+        removeFilesAfter ".shake" ["//*"]
