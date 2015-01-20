7e25706dc8cdc59a3ceb932576a27c806defaf2e
Adam Walker <adamwalker10@gmail.com>
2015-01-20 22:46:56 +1100

    three val overview figure

---
+++ diagrams/threeValOverview.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+import Diagrams.Prelude
+import Diagrams.Backend.Cairo.CmdLine
+
+diag = (text "T" <> (circle 5 # fc darkgray)) <> (circle 12 # scaleX 3.5 # alignR # fc gray) <> (circle 15 # scaleX 4 # alignR # fc lightgray # dashingN [0.03, 0.03] 0) <> (circle 20 # scaleX 4 # alignR) 
+
+main = mainWith (diag :: Diagram B R2)
