d2499d7d06dbcff34280b19ec68ca9cc76868f7b
Adam Walker <adamwalker10@gmail.com>
2015-04-22 21:42:10 +1000

    ASL in appendix

--- Shakefile.hs
+++ Shakefile.hs
@@ -8,7 +8,7 @@ import Development.Shake.Util
 
 buildDir = "build"
 
-latexSources  = ["thesis.tex", "background.tex", "game.tex", "intro.tex", "solving.tex", "syntcomp.tex", "userguided.tex", "i2c.tex", "conclusions.tex"]
+latexSources  = ["thesis.tex", "background.tex", "game.tex", "intro.tex", "solving.tex", "syntcomp.tex", "userguided.tex", "i2c.tex", "conclusions.tex", "asl.tex"]
 tslRefSources = ["body.tex"]
 bibSources    = ["extra.bib"]
 
